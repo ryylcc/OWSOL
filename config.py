794e357e2f5aa9bd3bb4607c64ef08649446cce8
ryylcc <85794259+ryylcc@users.noreply.github.com>
2023-04-19 13:46:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@
 # -----------------
 
 
-imagenet_root = '/raid/zhaochuan/master/NCL/code/wsolevaluation-master/dataset/ILSVRC'
-imagenet_meta = '/raid/zhaochuan/master/NCL/code/OWSOL/metadata/ImageNet'
-imagenet_sup_queue_path = '/raid/zhaochuan/master/NCL/code/OWSOL/metadata/ImageNet/sup_queue.pkl'
+imagenet_root = '../dataset/ILSVRC'
+imagenet_meta = '../metadata/ImageNet'
+imagenet_sup_queue_path = '../metadata/ImageNet/sup_queue.pkl'
 
-inatloc_root = '/raid/zhaochuan/master/NCL/code/wsolevaluation-master/dataset/iNatLoc'
-inatloc_meta = '/raid/zhaochuan/master/NCL/code/OWSOL/metadata/iNatLoc'
-inatloc_sup_queue_path = '/raid/zhaochuan/master/NCL/code/OWSOL/metadata/iNatLoc/scl_queue.pkl'
+inatloc_root = '../dataset/iNatLoc'
+inatloc_meta = '../metadata/iNatLoc'
+inatloc_sup_queue_path = '../metadata/iNatLoc/scl_queue.pkl'
 
-openimages_root = '/raid/zhaochuan/master/NCL/code/wsolevaluation-master/dataset/OpenImages'
-openimages_meta = '/raid/zhaochuan/master/NCL/code/OWSOL/metadata/OpenImages'
-openimages_sup_queue_path = '/raid/zhaochuan/master/NCL/code/OWSOL/metadata/OpenImages/scl_queue.pkl'
+openimages_root = '../dataset/OpenImages'
+openimages_meta = '../metadata/OpenImages'
+openimages_sup_queue_path = '../metadata/OpenImages/scl_queue.pkl'
 
 
 # -----------------
 # CKPT PATHS
 # -----------------
-pretrained_path = '/raid/zhaochuan/master/NCL/code/OWSOL/ckpt/moco_v2_200ep_pretrain.pth.tar'
+pretrained_path = '../ckpt/moco_v2_200ep_pretrain.pth.tar'
